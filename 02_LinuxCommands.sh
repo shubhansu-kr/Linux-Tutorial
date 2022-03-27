202684c6732fcda3fc50f8efc4532d48a7013075
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-27 20:34:52 +0530

    Linux shell

---
+++ 02_LinuxCommands.sh
@@ -0,0 +1,11 @@
+clear # Used to clear the terminal 
+
+echo Hello # Just echo backs the input
+
+echo "Hello - Using quotes is standard " 
+echo 'Single quote is also used '
+
+# What actually is terminal 
+
+# terminal runs a shell which is a language itself 
+# Different shells : bash , zsh ,
