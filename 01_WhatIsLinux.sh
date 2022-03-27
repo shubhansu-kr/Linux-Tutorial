bdd59f9f46758b8428fcac0ac5e63aa2100195e6
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-27 20:27:44 +0530

    What is linux

---
+++ 01_WhatIsLinux.sh
@@ -0,0 +1,22 @@
+echo Kernel - Hardware
+echo OS tools - Tool to interact with the hardware
+echo Distribution - Third party applications
+
+echo Linux Kernel
+
+echo Core of any Linux Distribution
+echo Bridge for software and hardware
+echo Manages hardware
+echo 
+echo TOOLs : eg - gcc compiler
+echo GNU is collection of these tools
+
+echo Linux Distribution
+echo Packages the os with package manager
+
+echo Example :
+echo Kali Linux
+echo Cent os
+echo Ubuntu
+echo Alpine
+echo 
