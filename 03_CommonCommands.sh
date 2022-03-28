64fb8a4c09cccbf7f6cffcd39d59f7226af8e864
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-28 11:59:44 +0530

    Common commands

---
+++ 03_CommonCommands.sh
@@ -0,0 +1,32 @@
+cd ${directory name} # change directory 
+pwd # present working directory 
+ls # list files and folders 
+
+ls -l # long list 
+ls -a # hidden files 
+ls -al # all files inclusing hidden files (long format)
+
+cat ${fileName} # is used to read a file in the terminal 
+
+cat /etc/os-release # Lists the os info 
+
+less ${fileName} # It is used to read big files 
+
+mkdir ${directoryName} # Ussed to make new directory 
+
+mkdir -p ${nestedDirectory} # -p is used to create nested library 
+
+rm ${fileName} # It used to delete files 
+
+rm -r ${folders} # Used to delete the directory 
+
+# cp is used to copy the files 
+
+# mv is used to move files and folders 
+
+# which command is used to find where binary file is like ls, cat etc 
+which ${Bin_Name} 
+
+# eg - which ls , which echo 
+
+# returns a path to the file 
