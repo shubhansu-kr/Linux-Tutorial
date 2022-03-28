b28ece7463b4f36ca6edd51c4c865f795b3ff8ef
shubhansu-kr <shubhansu2021@gmail.com>
2022-03-28 22:43:34 +0530

    Parameters

---
+++ 07_Parametes.sh
@@ -0,0 +1,8 @@
+# Parametes in script 
+# $1, $2, $3.. are used to pass parameters to the script 
+
+# This will echo the first parameter 
+echo 'First Parameter : '$1 
+
+# This will echo the second parameter 
+echo 'Second Parameter : '$2
